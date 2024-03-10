86309f27dd5d479cd36def4d15cb717c3376a099
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-03-10 16:51:52 +0200

    CSS task 5-6 add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBoxes = document.querySelector('#boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const inputNumber = document.querySelector('input[type="number"]');
+
+
+function createBoxes(amount) {
+  const newBox = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    newBox.push(box);
+    size += 10;
+  }
+  divBoxes.append(...newBox);
+}
+
+createBtn.addEventListener('click', (event) => {
+  const amount = parseInt(inputNumber.value);
+  if (amount > 1 && amount <= 100) {
+    createBoxes(amount);
+    amount.value = '';
+  }
+})
+
+destroyBtn.addEventListener('click', () => {
+  divBoxes.innerHTML = '';
+})
